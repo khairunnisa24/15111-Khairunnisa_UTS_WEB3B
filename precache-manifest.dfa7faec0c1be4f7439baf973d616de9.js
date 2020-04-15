9c557e4e212bb0737f9030124f3e615fea03b2b1
Khairunnisa <khairunnisa00@mail.ugm.ac.id>
2020-04-15 16:07:55 +0700

    Updates

---
+++ precache-manifest.dfa7faec0c1be4f7439baf973d616de9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b22d74032fca0dc69cfc8c49fdb8874",
+    "url": "/15111-Khairunnisa_UTS_WEB3B/index.html"
+  },
+  {
+    "revision": "f22165bc342aa37cbb93",
+    "url": "/15111-Khairunnisa_UTS_WEB3B/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "56d09dffd490b570215e",
+    "url": "/15111-Khairunnisa_UTS_WEB3B/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f22165bc342aa37cbb93",
+    "url": "/15111-Khairunnisa_UTS_WEB3B/static/js/2.49fd3eb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/15111-Khairunnisa_UTS_WEB3B/static/js/2.49fd3eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d09dffd490b570215e",
+    "url": "/15111-Khairunnisa_UTS_WEB3B/static/js/main.3b21dae4.chunk.js"
+  },
+  {
+    "revision": "05e50b50cfd9b7bb906b",
+    "url": "/15111-Khairunnisa_UTS_WEB3B/static/js/runtime-main.4f29bcd3.js"
+  }
+]);
